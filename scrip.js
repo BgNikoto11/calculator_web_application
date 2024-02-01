c1a6905037e1f02169fd6e2e44ddd0c8bf984fa9
BgNikoto11 <nikolamilanov2005@gmail.com>
2024-02-01 16:27:16 +0200

    improved readability and functionality

--- scrip.js
+++ scrip.js
@@ -1,41 +1,44 @@
-function display(buttonValue){
-    document.getElementById("calculator-display").value += buttonValue;
-}
-function displayKeyboard(event){
-    if(event.key == '1' || event.key == '2'
-    || event.key == '3' || event.key == '4'
-    || event.key == '5' || event.key == '6'
-    || event.key == '7' || event.key == '8'
-    || event.key == '9' || event.key == '+'
-    || event.key == '-' || event.key == '/'
-    || event.key == '*' || event.key == '.')
-    document.getElementById("calculator-display").value += event.key;
+var calculatorDisplay = document.getElementById("calculator-display");
 
+function display(buttonValue) {
+    calculatorDisplay.value += buttonValue;
 }
 
-function solve(){
-    let x = document.getElementById("calculator-display").value;
-    let y = math.evaluate(x);
-    document.getElementById("calculator-display").value = y;
-}
-
-function clearAll(){
-    document.getElementById("calculator-display").value = "";
-}
-
-function deleteOne(){
-let x = document.getElementById("calculator-display").value;
-let y = x.slice(0, -1);
-document.getElementById("calculator-display").value = y;
-}
-
-document.onkeydown = function(event){
-    if (event.key === "Enter"){
-        console.log("Enter"); 
-        event.preventDefault();
-        solve(); 
+function displayKeyboard(event) {
+    if (event.key == '1' || event.key == '2'
+        || event.key == '3' || event.key == '4'
+        || event.key == '5' || event.key == '6'
+        || event.key == '7' || event.key == '8'
+        || event.key == '9' || event.key == '+'
+        || event.key == '-' || event.key == '/'
+        || event.key == '*' || event.key == '.') {
+        calculatorDisplay.value += event.key;
     }
-    else if(event.key === "Backspace"){
+}
+document.addEventListener('keydown', displayKeyboard);
+
+function solve() {
+    let oldValue = calculatorDisplay.value;
+    let newValue = math.evaluate(oldValue);
+    calculatorDisplay.value = newValue;
+}
+
+function clearAll() {
+    calculatorDisplay.value = "";
+}
+
+function deleteOne() {
+    let oldValue = calculatorDisplay.value;
+    let newValue = oldValue.slice(0, -1);
+    calculatorDisplay.value = newValue;
+}
+
+document.onkeydown = function (event) {
+    if (event.key === "Enter") {
+        console.log("Enter");
+        event.preventDefault();
+        solve();
+    } else if (event.key === "Backspace") {
         console.log("Backspace");
         deleteOne();
     }
